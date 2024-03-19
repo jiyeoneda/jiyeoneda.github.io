480d8b7917a2550171a2248c1c4653e0c42c28f0
jiyeoneda <163019217+jiyeoneda@users.noreply.github.com>
2024-03-19 21:43:47 +0900

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	float k, c, dt, t, num, anal;
+	//f(t) : log (k * t + c)
+	//미분
+	cout << "k를 입력하세요: ";
+	cin >> k;
+	cout << "c를 입력하세요: ";
+	cin >> c;
+	cout << "t를 입력하세요: ";
+	cin >> t;
+	cout << "dt를 입력하세요: ";
+	cin >> dt;
+	anal =  k * (1 / (k * t + c));
+	num = (log(k * (t + dt) + c) - log(k * t + c))/ dt;
+	cout << "num: " << num << " anal: " << anal << endl;
+	cout << " error = " << (num - anal) / anal * 100 << "%\n";
+	//적분
+	float a, b;
+	int N;
+	cout << "a를 입력하세요: ";
+	cin >> a;
+	cout << "b를 입력하세요: ";
+	cin >> b;
+	cout << "N를 입력하세요: ";
+	cin >> N;
+	anal = (1 / k) * (((k * b + c) * (log(k * b + c) - 1)) - ((k * a + c) * (log(k * a + c) - 1)));
+	dt = (b - a) / N;
+	num = 0;
+	for (int i = 0; i < N; i++) {
+		num += log((k * (a + dt * i)) + c) * dt;
+	}
+	cout << "num: " << num << " anal: " << anal << endl;
+	cout << " error = " << (num - anal) / anal * 100 << "%\n";
+
+}
